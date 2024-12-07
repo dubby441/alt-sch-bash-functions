7173c0959c0dee3b39fd6e79962249e46f144058
dubby441 <dubby441@gmail.com>
2024-12-07 17:23:38 +0000

    "chidubem's assignment"

---
+++ chidubem.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Predefined user information
+full_name="Chidubem Harrison Okpala"
+
+bio="My student ID: ALT/SOE/024/1168. I am from Anambra state in Nigeria and studied electrical engineering in Anambra state univeristy.Currently I am in Portugal where doing my masters degree in data driven marketing in specialization in data science."
+
+goals="My goals for the school of engineering programme"
+        "To study all that has been taught to better develop myself"
+        "Collaborate with other students to understand better"
+        "Finish the programme with flying colours"
+        "I want to be an outstanding tech bro"
+
+years_in_tech="I am a newbie who wants to break into the tech world"
+
+dream_job="Working for one of the biggest tech company"
+
+# Displaying the predefined information
+echo -e "\nUser Details:"
+echo "---------------------------------"
+echo "Full Name     : $full_name"
+echo "Bio           : $bio"
+echo "Goals         : $goals"
+echo "Years in Tech : $years_in_tech"
+echo "---------------------------------"
